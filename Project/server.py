5eb4058b0513903639c1b34a75e6913976015ecb
Eckods <eckods@hotmail.com>
2017-11-12 21:38:22 -0800

    Command retrieval works
    But data channel connection does not

--- Project/server.py
+++ Project/server.py
@@ -1,6 +1,6 @@
 # Authors: Steve Sanchez, ???, ???
 # Course: CPSC 471
-# Due date: 11/16/17
+# Due date: 11/18/17
 # This program INSERT SOME DESCRIPTION
 #
 
@@ -24,14 +24,95 @@ ctrl_channel = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ctrl_channel.bind(('', ctrl_port))
 ctrl_channel.listen(1)
 
-# control channel is client to server connection for getting commands. Terminate at end/ quit command
-# sooo, when server receives a quit command, server closes connection then client recognizes
-# and closes as well.
+# ************************************************
+# Receives the specified number of bytes
+# from the specified socket
+# @param sock - the socket from which to receive
+# @param numBytes - the number of bytes to receive
+# @return - the bytes received
+# *************************************************
+def recvAll(sock, numBytes):
 
-# data channel is used for transferring data...generates new port for client and server 
-# sooo, when user puts in a ls, get or put command, client creates ephemeral port for connection
-# and then server connects to that new port for transfer to be done. Then that channel is closed
-# Note: server has terminated NEW connection, NOT existing one for listening to client commands
+	# The buffer
+	recvBuff = ""
+	
+	# The temporary buffer
+	tmpBuff = ""
+	
+	# Keep receiving till all is received
+	while len(recvBuff) < numBytes:
+		
+		# Attempt to receive bytes
+		tmpBuff =  sock.recv(numBytes)
+		
+		# The other side has closed the socket
+		if not tmpBuff:
+			break
+		
+		# Add the received bytes to the buffer
+		recvBuff += tmpBuff
+	
+	return recvBuff
+    
+def receiveData(command_sock):
+	# The buffer to all data received from the the client.
+	fileData = ""
+	
+	# The temporary buffer to store the received data.
+	recvBuff = ""
+	
+	# The size of the incoming file
+	fileSize = 0	
+	
+	# The buffer containing the file size
+	fileSizeBuff = ""
+	
+	# Receive the first 10 bytes indicating the size of the file
+	fileSizeBuff = recvAll(command_sock, 10)
+	
+	# Get the file size
+	fileSize = int(fileSizeBuff)
+
+	print ("The file size is ", fileSize)
+	
+	# Get the file data
+	fileData = recvAll(command_sock, fileSize)
+	
+	print ("The file data is: ")
+	print (fileData)
+
+	return fileData
+
+
+'''def sendData(args, conn_channel):
+    # Get the size of the data read and convert it to string
+    dataSizeStr = str(len(args))
+    print(dataSizeStr)
+    print(len(args))
+    
+    # Prepend 0's to the size string until the size is 10 bytes
+    while len(dataSizeStr) < 10:
+         dataSizeStr = "0" + dataSizeStr
+         
+    # Prepend the size of the data to the file data.
+    args = dataSizeStr + args
+
+    # The number of bytes sent
+    numSent = 0
+    
+    # Send the data!
+    while len(args) > numSent:
+        numSent += conn_channel.send(args[numSent:])
+
+def sendFile(args):
+    # Read file data
+    fileObj = open(args[1], "r")
+    fileData = fileObj.read()
+        
+    # Make sure we did not hit EOF
+    if fileData:
+        sendData(fileData)
+'''
 
 while True:
     print("Awaiting connections...")
@@ -41,52 +122,47 @@ while True:
 
     print("Accepted connection from client: ", command_addr)
     
-    ''' INSERT CODE TO RETRIEVE COMMANDS/DATA FROM CLIENT HERE'''
+    command = receiveData(command_sock)
+    print("command is", command)
     
-    # If the client input a command and argument, then determine if get or put
-    if (len(client_args) == 2):
-        if(client_args[0] == 'get'):
-            # Connect to ephemeral port for data transfer
-            data_channel = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            data_channel.connect(('',0))
-            
+    # If command is ls, get, or put, then connect to ephemeral port for data transfer
+    if (command == 'ls' or command == 'get' or command == 'put'):
+        data_channel = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        data_channel.connect(('0.0.0.0',0))
+
+
+        # If the command is get, send over the file requested
+        if (command == 'get'):
             print("get check works")
-        elif(client_args[0] == 'put'):
-            # Connect to ephemeral port for data transfer
-            data_channel = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            data_channel.connect(('',0))
-            
+
+        # Else if the command is put, retrieve the file from the client and store
+        elif(command == 'put'):
             print("put check works")
+
+        # Else the command is ls so display items in directory
         else:
-            print("ERROR! Invalid input given.")
-            print("Usage: get <filename> OR put <filename>")
-
-    # Else if the client input only a command, then determine if ls or quit
-    elif (len(client_args) == 1):
-        if(client_args[0] == 'ls'):
-            # Connect to ephemeral port for data transfer
-            data_channel = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            data_channel.connect(('',0))
-
             # Run ls-equivalent command for Windows
             if os.name == 'nt':
                 subprocess.call('dir', shell=True) # SOMEHOW NEED TO STORE THIS IN A STRING TO PASS TO CLIENT
+
             # Else run ls -l for Linux
             elif os.name == 'posix':
                 subprocess.call(['ls', '-l'], shell=True) # SOMEHOW NEED TO STORE THIS IN A STRING TO PASS TO CLIENT
-                
-            '''INSERT CODE TO SEND DATA TO CLIENT'''
-            
-            # Close data channel connection
-            data_channel.close()
-        elif (client_args[0] == 'quit'):
-            # Close server side of connection
-            command_sock.close()
-            break
-        else:
-            print("ERROR! Invalid input given.")
-            print("Usage: ls OR quit")
-            
-time.sleep(4.5)
+
+        # Close the data channel connection
+        data_channel.close()
+        
+    # Else if the command is quit, prepare to close connection
+    elif (command == 'quit'):
+        # Close server side of connection
+        command_sock.close()
+        break
+    
+    # Else the command is not recognized so print error message
+    else:
+        print("FAILURE. Invalid input given.")
+        print("Usage: get <filename> OR put <filename>")
+        print("Usage: ls OR quit")
+          
 # End server
 ctrl_channel.close()
