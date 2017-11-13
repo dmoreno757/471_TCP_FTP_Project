7c953791e8236cc07ce50df3d150a721d64c9533
Eckods <eckods@hotmail.com>
2017-11-12 21:37:39 -0800

    Got commands sending over command channel
    Data channel via ephemeral port doesn't work though

--- Project/client.py
+++ Project/client.py
@@ -1,6 +1,6 @@
 # Authors: Steve Sanchez, ???, ???
 # Course: CPSC 471
-# Due date: 11/16/17
+# Due date: 11/18/17
 # This program INSERT SOME DESCRIPTION
 #
 
@@ -31,14 +31,94 @@ ctrl_channel.connect((serv_IP, serv_port))
 print(serv_name, serv_port)
 print(serv_IP)
 
-# control channel is client to server connection for getting commands. Terminate at end/ quit command
-# sooo, when server receives a quit command, server closes connection then client recognizes
-# and closes as well.
+def sendData(args, conn_channel):
+    # Get the size of the data read and convert it to string
+    dataSizeStr = str(len(args))
+    print(dataSizeStr)
+    print(len(args))
+    
+    # Prepend 0's to the size string until the size is 10 bytes
+    while len(dataSizeStr) < 10:
+         dataSizeStr = "0" + dataSizeStr
+         
+    # Prepend the size of the data to the file data.
+    args = dataSizeStr + args
 
-# data channel is used for transferring data...generates new port for client and server 
-# sooo, when user puts in a ls, get or put command, client creates ephemeral port for connection
-# and then server connects to that new port for transfer to be done. Then that channel is closed
-# Note: server has terminated NEW connection, NOT existing one for listening to client commands
+    # The number of bytes sent
+    numSent = 0
+    
+    # Send the data!
+    while len(args) > numSent:
+        numSent += conn_channel.send(args[numSent:])
+
+def sendFile(args):
+    # Read file data
+    fileObj = open(args[1], "r")
+    fileData = fileObj.read()
+        
+    # Make sure we did not hit EOF
+    if fileData:
+        sendData(fileData)
+'''
+# ************************************************
+# Receives the specified number of bytes
+# from the specified socket
+# @param sock - the socket from which to receive
+# @param numBytes - the number of bytes to receive
+# @return - the bytes received
+# *************************************************
+def recvAll(sock, numBytes):
+
+	# The buffer
+	recvBuff = ""
+	
+	# The temporary buffer
+	tmpBuff = ""
+	
+	# Keep receiving till all is received
+	while len(recvBuff) < numBytes:
+		
+		# Attempt to receive bytes
+		tmpBuff =  sock.recv(numBytes)
+		
+		# The other side has closed the socket
+		if not tmpBuff:
+			break
+		
+		# Add the received bytes to the buffer
+		recvBuff += tmpBuff
+	
+	return recvBuff
+    
+def receiveData(command_sock):
+	# The buffer to all data received from the the client.
+	fileData = ""
+	
+	# The temporary buffer to store the received data.
+	recvBuff = ""
+	
+	# The size of the incoming file
+	fileSize = 0	
+	
+	# The buffer containing the file size
+	fileSizeBuff = ""
+	
+	# Receive the first 10 bytes indicating the size of the file
+	fileSizeBuff = recvAll(command_sock, 10)
+	
+	# Get the file size
+	fileSize = int(fileSizeBuff)
+
+	print ("The file size is ", fileSize)
+	
+	# Get the file data
+	fileData = recvAll(command_sock, fileSize)
+	
+	print ("The file data is: ")
+	print (fileData)
+
+	return fileData
+ '''
 
 while True:
     # Get input from the user
@@ -47,7 +127,7 @@ while True:
 
     # Split the input into the command + arguments(if any)
     client_args = line.split()
-
+    
     # First check if ls,get,or put commands to generate an ephemeral port for transfer 
     if (client_args[0] == 'ls' or client_args[0] == 'get' or client_args[0] == 'put'):
         # Create a socket used for data transfer
@@ -56,23 +136,23 @@ while True:
         # Bind the socket to port 0 and listen for response
         data_channel.bind(('',0))
 
-        '''INSERT CODE FOR SENDING COMMANDS/DATA TO SERVER HERE'''
+        # Send commands over control channel
+        sendData(client_args[0], ctrl_channel)
         
-        data_channel.listen(1)
-            while True:
-                '''INSERT CODE FOR RETRIEVING DATA FROM SERVER HERE'''
-                    '''Somehow detect its done sending in which case, break'''
-                break
+        data_channel.listen(2)
+        while 2:
+            print("Established data channel connection")
+            
         data_channel.close()
+        
     else:
-        # Send client input to server
-        '''INSERT CODE FOR SENDING COMMANDS/DATA TO SERVER HERE'''
+        # Send commands over control channel
+        sendData(client_args[0], ctrl_channel)
 
         # If command is quit, prepare to close connection
         if (client_args[0] == 'quit'):
             # Prepare to close connection
             break
-    
-time.sleep(4.5)
+
 # End client
 ctrl_channel.close()
